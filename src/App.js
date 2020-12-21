70409c2ecef9cdfdf108726bc06fbc683375a80e
SSaif98 <saifali7522@gmail.com>
2020-12-21 18:15:30 +0500

    git repo api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect(()=>{  
+
+  async function getRepos(){
+      const response = await fetch( 'https://api.github.com/users/SSAIF98/repos');
+      const data = await response.json();
+      console.log(data);
+      setRepos(data);
+    }
+    getRepos(); 
+  },[]);
+
+  //   'https://api.github.com/users/SSAIF98/repos'
+  //   fetch('https://jsonplaceholder.typicode.com/posts/1')
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     setData(json)
+  //   });
+  // },[])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* {data.title} */}
+    <ul>
+      {repos.map((repoObj, ind) => {
+        return (<li key={ind}>
+          {repoObj.name}
+        </li>)
+      })}
+    </ul>
     </div>
   );
 }
